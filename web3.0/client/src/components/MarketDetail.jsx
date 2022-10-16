c79f92d9b4c757622b4395d2d1f294c9e3a32bd7
datmanlyman <jeewern2@gmail.com>
2022-10-16 16:28:53 +0800

    left right done(?)

--- web3.0/client/src/components/MarketDetail.jsx
+++ web3.0/client/src/components/MarketDetail.jsx
@@ -64,9 +64,32 @@ const MarketDetail = () => {
                     </div>
                 </div>
 
-                <div className=" mt-6 w-full p-5 bg-zinc-900 max-h-full rounded overflow-hidden shadow-lg">
+                <div className=" mt-6 w-full p-5 bg-zinc-900 max-h-full rounded overflow-hidden shadow-lg grid grid-cols-0 justify-between">
                     <h1 className="text-4xl text-white sm:text-2xl">Buy and Sell</h1>
-                    <div className="mt-6 w-full p-5 bg-red-900 max-h-full rounded overflow-hidden shadow-lg">
+                    <div className="mt-16 w-11/12 p-5 bg-sky-900 max-h-full rounded overflow-hidden shadow-lg col-start-1">
+                        <h1 className="text-4xl text-white bg sm:text-2xl">Y-Token</h1>
+                        <p className="font-mono pt-3 text-xl text-white">You own: [number here] Y-Tokens</p>
+                        <h1 className="text-xl pt-3 text-white bg sm:text-xl">I want to buy..</h1>
+                        <div className='flexalign-middle'>
+                            <Input name="YTokenAmt" className=" w-auto bg-transparent text-xl pt-3 text-white placeholder:font-normal placeholder:italic placeholder:text-blue-400" value="1" placeholder="1" type="number" />
+                            <span className='text-xl pt-3 text-white '> Y-Token(s)</span>
+                            <p className=" font-mono pt-3 text-white">Price of Y-Token <span className=' text-blue-300'>(Option:{currentMarket.sides[1]})</span>: {currentMarket.Y_Price} ETH each</p>
+                            <p className=" font-mono text-white">Quanity to buy: [value here]</p>
+                            <button className="bg-blue-500 w-24 text-white my-8 pt-2 pb-2 pl-3 pr-3 font-mono rounded shadow-sky-700 shadow-lg">BUY</button>
+                        </div>
+
+                        <hr></hr>
+                        <h1 className="text-xl pt-3 text-white bg sm:text-xl">I want to sell..</h1>
+                        <div className='flexalign-middle'>
+                            <Input name="YTokenAmt" className=" w-auto bg-transparent text-xl pt-3 text-white placeholder:font-normal placeholder:italic placeholder:text-blue-400" value="1" placeholder="1" type="number" />
+                            <span className='text-xl pt-3 text-white '> Y-Token(s)</span>
+
+                            <p className=" font-mono pt-3 text-white">Price of Y-Token <span className=' text-blue-300'>(Option:{currentMarket.sides[1]})</span>: {currentMarket.Y_Price} ETH each</p>
+                            <p className=" font-mono text-white">Quanity to sell: [value here]</p>
+                            <button className="bg-red-500 w-24 text-white my-8 pt-2 pb-2 pl-3 pr-3 font-mono rounded shadow-pink-700 shadow-lg">SELL</button>
+                        </div>
+                    </div>
+                    <div className="mt-16 w-11/12 p-5 bg-red-900 max-h-full rounded overflow-hidden shadow-lg col-start-13">
                         <h1 className="text-4xl text-white sm:text-2xl">N-Token</h1>
                         <p className="font-mono pt-3 text-xl text-white">You own: [number here] N-Tokens</p>
                         <h1 className="text-xl pt-3 text-white bg sm:text-xl">I want to buy..</h1>
@@ -91,29 +114,6 @@ const MarketDetail = () => {
                             <button className="bg-red-500 w-24 text-white my-8 pt-2 pb-2 pl-3 pr-3 font-mono rounded shadow-pink-700 shadow-lg">SELL</button>
                         </div>
 
-                    </div>
-                    <div className="mt-6 w-full p-5 bg-sky-900 max-h-full rounded overflow-hidden shadow-lg">
-                        <h1 className="text-4xl text-white bg sm:text-2xl">Y-Token</h1>
-                        <p className="font-mono pt-3 text-xl text-white">You own: [number here] Y-Tokens</p>
-                        <h1 className="text-xl pt-3 text-white bg sm:text-xl">I want to buy..</h1>
-                        <div className='flexalign-middle'>
-                            <Input name="YTokenAmt" className=" w-auto bg-transparent text-xl pt-3 text-white placeholder:font-normal placeholder:italic placeholder:text-blue-400" value="1" placeholder="1" type="number" />
-                            <span className='text-xl pt-3 text-white '> Y-Token(s)</span>
-                            <p className=" font-mono pt-3 text-white">Price of Y-Token <span className=' text-blue-300'>(Option:{currentMarket.sides[1]})</span>: {currentMarket.Y_Price} ETH each</p>
-                            <p className=" font-mono text-white">Quanity to buy: [value here]</p>
-                            <button className="bg-blue-500 w-24 text-white my-8 pt-2 pb-2 pl-3 pr-3 font-mono rounded shadow-sky-700 shadow-lg">BUY</button>
-                        </div>
-
-                        <hr></hr>
-                        <h1 className="text-xl pt-3 text-white bg sm:text-xl">I want to sell..</h1>
-                        <div className='flexalign-middle'>
-                            <Input name="YTokenAmt" className=" w-auto bg-transparent text-xl pt-3 text-white placeholder:font-normal placeholder:italic placeholder:text-blue-400" value="1" placeholder="1" type="number" />
-                            <span className='text-xl pt-3 text-white '> Y-Token(s)</span>
-
-                            <p className=" font-mono pt-3 text-white">Price of Y-Token <span className=' text-blue-300'>(Option:{currentMarket.sides[1]})</span>: {currentMarket.Y_Price} ETH each</p>
-                            <p className=" font-mono text-white">Quanity to sell: [value here]</p>
-                            <button className="bg-red-500 w-24 text-white my-8 pt-2 pb-2 pl-3 pr-3 font-mono rounded shadow-pink-700 shadow-lg">SELL</button>
-                        </div>
                     </div>
                 </div>
 
