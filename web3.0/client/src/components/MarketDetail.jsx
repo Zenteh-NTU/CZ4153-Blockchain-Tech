365df86e92516d33476e134f6493be2044f0e9b3
zen <hteh002@e.ntu.edu.sg>
2022-10-30 13:04:22 +0800

    UI Fix

--- web3.0/client/src/components/MarketDetail.jsx
+++ web3.0/client/src/components/MarketDetail.jsx
@@ -85,15 +85,15 @@ const MarketDetail = () => {
 
     return (
         <div className="w-full flex justify-center min-h-screen bg-gradient-to-r from-blue-900 to-pink-900">
-            <div className="pr-0 pt-32 lg:pr-96 ">
+            <div className="pt-32 ">
                 <h1 className="text-4xl text-white sm:text-5xl">{currentMarket.marketName}</h1>
-                <h1 className="text-white">Winning Bet: { winning }</h1>
+                <h1 className="text-white mt-1">Winning Bet Result: { winning }</h1>
                 <div className="pt-4">
-                    <span className="inline-block bg-slate-900 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Outcome Date: {outcomeDate(currentMarket.resultDate)}</span>
+                    <span className="inline-block bg-slate-900 rounded-full px-3 py-1 text-sm font-semibold text-white mb-2">Outcome Date: {outcomeDate(currentMarket.resultDate)}</span>
                 </div>
 
                 <div>
-                    <span className="inline-block bg-slate-900 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Market Close Date: {marketCloseDate(currentMarket.resultDate)}</span>
+                    <span className="inline-block bg-slate-900 rounded-full px-3 py-1 text-sm font-semibold text-white mb-2">Market Close Date: {marketCloseDate(currentMarket.resultDate)}</span>
                 </div>
 
                 <div className=" mt-6 w-full p-5 bg-zinc-900 max-h-full rounded overflow-hidden shadow-lg">
@@ -112,37 +112,36 @@ const MarketDetail = () => {
 
                     <p className=" font-mono pt-3 text-white">Price of Y-Token <span className=' text-blue-300'>(Option:{currentMarket.sides[1]})</span>: {(parseFloat(currentMarket.Y_Price).toFixed(3)).toString()} ETH each</p>
                     <p className=" font-mono pt-3 text-white">Price of N-Token <span className=' text-red-400'>(Option:{currentMarket.sides[0]})</span>: {(parseFloat(currentMarket.N_Price).toFixed(3)).toString()} ETH each</p>
+                    <p className=" font-mono pt-3 text-white">Prize Pool: { (parseFloat(currentMarket.contractBalance).toFixed(3)).toString() } ETH</p>
                     <hr className='mt-4'></hr>
                     <div className="pt-4">
-                        <span className="inline-block bg-slate-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Market Hash: {currentMarket.contractHash}</span>
+                        <span className="inline-block bg-slate-700 rounded-full px-3 py-1 text-sm font-semibold text-white mb-2">Market Hash: {currentMarket.contractHash}</span>
                     </div>
                     <div>
-                        <span className="inline-block bg-slate-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Created by: {currentMarket.ownerHash}</span>
+                        <span className="inline-block bg-slate-700 rounded-full px-3 py-1 text-sm font-semibold text-white mb-2">Created by: {currentMarket.ownerHash}</span>
                     </div>
                 </div>
 
-                <div className=" mt-6 w-full p-5 bg-zinc-900 max-h-full rounded overflow-hidden shadow-lg grid grid-cols-0 justify-between">
-                    <div>
-                        <h1 className='text-4xl text-white sm:text-2xl'>Prize Pool: { (parseFloat(currentMarket.contractBalance).toFixed(3)).toString() } ETH</h1>
-                    </div>
+                <div className=" mt-6 w-full  bg-zinc-900 max-h-full rounded overflow-hidden shadow-lg flex flex-col items-center">
                     <br/>
-                    <h1 className="text-4xl text-white sm:text-2xl">Buy and Sell</h1>
-                    <div className="mt-16 w-11/12 p-5 bg-sky-900 max-h-full rounded overflow-hidden shadow-lg col-start-1">
+                    <h1 className="p-5 text-white text-2xl">Buy and Sell</h1>
+                    <div className="flex flex-col lg:flex-row">
+                    <div className="m-5 p-5 lg:ml-5 lg:mr-0 lg:mt-5 lg:mb-5 bg-sky-900 max-h-full rounded overflow-hidden shadow-lg">
                         <button className="text-white" onClick={ () => winnerYTokens() }>Choose as Winner</button>
-                        <h1 className="text-4xl text-white bg sm:text-2xl">Y-Token</h1>
-                        <p className="font-mono pt-3 text-xl text-white">You own: { currentMarket.UserYToken } Y-Tokens</p>
+                        <h1 className="text-white bg text-2xl">Y-Token</h1>
+                        <p className="pt-3 text-xl text-white">You own: { currentMarket.UserYToken } Y-Tokens</p>
                         <h1 className="text-xl pt-3 text-white bg sm:text-xl">I want to buy..</h1>
                         <div className='flexalign-middle'>
                             <Input name="YTokenAmt" handleChange={ handleChange1 } value={ value1 } className=" w-auto bg-transparent text-xl pt-3 text-white placeholder:font-normal placeholder:italic placeholder:text-blue-400" placeholder="Enter the number of Y-tokens..." type="text" id="buyY" />
                             <span className='text-xl pt-3 text-white '> Y-Token(s)</span>
-                            <p className=" font-mono pt-3 text-white">Price of Y-Token <span className=' text-blue-300'>(Option:{currentMarket.sides[1]})</span>: {(parseFloat(currentMarket.Y_Price).toFixed(3)).toString()} ETH each</p>
-                            <p className=" font-mono text-white">Quanity to buy: { value1 }</p>
+                            <p className="pt-3 text-white">Price of Y-Token <span className=' text-blue-300'>(Option:{currentMarket.sides[1]})</span>: {(parseFloat(currentMarket.Y_Price).toFixed(3)).toString()} ETH each</p>
+                            <p className=" text-white">Quanity to buy: { value1 }</p>
                             <button className="bg-blue-500 w-24 text-white my-8 pt-2 pb-2 pl-3 pr-3 font-mono rounded shadow-sky-700 shadow-lg" onClick={ () => handleSubmit("buyY", currentMarket.contractHash, currentMarket.Y_Price, value1) }>BUY</button>
                         </div>
 
                         <hr></hr>
                         <h1 className="text-xl pt-3 text-white bg sm:text-xl">I want to sell..</h1>
-                        <div className='flexalign-middle'>
+                        <div className='align-middle'>
                             <Input name="YTokenAmt" handleChange={ handleChange2 } value={ value2 } className=" w-auto bg-transparent text-xl pt-3 text-white placeholder:font-normal placeholder:italic placeholder:text-blue-400" placeholder="Enter the number of Y-tokens..." type="text" id="sellY" />
                             <span className='text-xl pt-3 text-white '> Y-Token(s)</span>
 
@@ -151,12 +150,12 @@ const MarketDetail = () => {
                             <button className="bg-red-500 w-24 text-white my-8 pt-2 pb-2 pl-3 pr-3 font-mono rounded shadow-pink-700 shadow-lg" onClick={ () => handleSubmit("sellY", currentMarket.contractHash, currentMarket.Y_Price, value2) }>SELL</button>
                         </div>
                     </div>
-                    <div className="mt-16 w-11/12 p-5 bg-red-900 max-h-full rounded overflow-hidden shadow-lg col-start-13">
+                    <div className="m-5 p-5 bg-red-900 max-h-full rounded overflow-hidden shadow-lg col-start-13">
                         <button className="text-white" onClick={ () => winnerNTokens() }>Choose as Winner</button>
-                        <h1 className="text-4xl text-white sm:text-2xl">N-Token</h1>
-                        <p className="font-mono pt-3 text-xl text-white">You own: { currentMarket.UserNToken } N-Tokens</p>
+                        <h1 className="text-white text-2xl">N-Token</h1>
+                        <p className="pt-3 text-xl text-white">You own: { currentMarket.UserNToken } N-Tokens</p>
                         <h1 className="text-xl pt-3 text-white bg sm:text-xl">I want to buy..</h1>
-                        <div className='flexalign-middle'>
+                        <div className='align-middle'>
                             <Input name="YTokenAmt" handleChange={ handleChange3 } value={ value3 } className=" w-auto bg-transparent text-xl pt-3 text-white placeholder:font-normal placeholder:italic placeholder:text-blue-400" placeholder="Enter the number of N-tokens..." type="text" id="buyN" />
                             <span className='text-xl pt-3 text-white '> N-Token(s)</span>
 
@@ -177,9 +176,10 @@ const MarketDetail = () => {
                             <button className="bg-red-500 w-24 text-white my-8 pt-2 pb-2 pl-3 pr-3 font-mono rounded shadow-pink-700 shadow-lg" onClick={ () => handleSubmit("sellN", currentMarket.contractHash, currentMarket.N_Price, value4) }>SELL</button>
                         </div>
                     </div>
-                    <div>
+                    </div>
+                    <div className='flex flex-col mt-12 mb-24 items-center text-center w-96'>
                         <h2 className='text-white'>Claim your gains, if any, after the winner has been anounced</h2>
-                        <button className='text-white' type='submit' onClick={ () => withdrawGains() }>Claim</button>
+                        <button className='bg-blue-500 w-24 mt-12 text-white my-8 pt-2 pb-2 pl-3 pr-3 font-mono rounded shadow-sky-700 shadow-lg' type='submit' onClick={ () => withdrawGains() }>Claim</button>
                     </div>
                 </div>
             </div>
--- web3.0/client/src/utils/constants.js
+++ web3.0/client/src/utils/constants.js
@@ -5,4 +5,4 @@ import TransactionsABI from './Transactions.json';
 export const marketTrackerContractABI = MarketTrackerABI.abi;
 export const marketContractABI = MarketABI.abi;
 export const transactionsABI = TransactionsABI.abi;
-export const contractAddress = '0xCdB95A91965C46aF2caEB3B4105cc97cD4b17401';
+export const contractAddress = '0x4250C5e075144d00B89b350651809d774feb5a6D';
